7129f128c9a29831ba3b3f223a6ace615d951e24
galwort <wthomasgorbett@gmail.com>
2023-04-08 10:19:51 -0400

    Flip polygon vertically

--- kpi-logo.py
+++ kpi-logo.py
@@ -6,13 +6,13 @@ def make_cube(image, x, y, size=20, color=(255, 255, 255), outline=(0, 0, 0), sh
     
     cube_coords = [
         (x, y),                         # Middle
-        (x, y - size * 4),              # Middle-bottom
-        (x + size * 3, y - size * 3),   # Right-bottom
-        (x + size * 3, y + size),       # Right-top
+        (x + size * 3, y - size),       # Top-right
+        (x + size * 3, y + size * 3),   # Bottom-right
+        (x, y + size * 4),              # Bottom-middle
     ]
 
     draw.polygon(cube_coords[:4], fill=color, outline=outline) # Front face
 
     img.save('kpi-logo.png')
 
-make_cube('kpi-logo.png', 100, 100)
+make_cube('kpi-logo.png', 200, 200)
