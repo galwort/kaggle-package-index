f1559e9727db995fc4c1eeb6cf78ac9300c54778
galwort <wthomasgorbett@gmail.com>
2023-04-08 13:45:47 -0400

    Test radians function

--- kpi-logo.py
+++ kpi-logo.py
@@ -1,5 +1,5 @@
 from PIL import Image, ImageDraw
-
+from math import cos, radians
 
 def make_cube(
     image,
@@ -9,23 +9,24 @@ def make_cube(
     color=(255, 255, 255),
     outline=(0, 0, 0),
     shaded=False,
-    vertical_perspective=45,
+    vertical_perspective=57.29577951308232,
 ):
     img = Image.open(image)
     draw = ImageDraw.Draw(img)
 
-    side = size * 4
+    edge = size * 2
+    print(radians(vertical_perspective))
 
     cube_coords = [
         (x, y),  # Middle
         (x + size * 3, y - size),  # Top-right
         (x + size * 3, y + size * 3),  # Bottom-right
-        (x, y + side),  # Bottom-middle
+        (x, y + edge),  # Bottom-middle
     ]
 
     draw.polygon(cube_coords[:4], fill=color, outline=outline)  # Front face
 
-    img.save("kpi-logo.png")
+    # img.save("kpi-logo.png")
 
 
 make_cube("kpi-logo.png", 200, 200)
