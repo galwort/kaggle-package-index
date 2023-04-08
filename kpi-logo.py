e45a4f2cd80c1956286da7235800ea57c927e7d2
galwort <wthomasgorbett@gmail.com>
2023-04-08 12:13:32 -0400

    Create vertical perspective variable

--- kpi-logo.py
+++ kpi-logo.py
@@ -3,12 +3,15 @@ from PIL import Image, ImageDraw
 def make_cube(image, x, y, size=20, color=(255, 255, 255), outline=(0, 0, 0), shaded=False):
     img = Image.open(image)
     draw = ImageDraw.Draw(img)
+
+    side = size * 4
+    vert_perp = 45
     
     cube_coords = [
         (x, y),                         # Middle
         (x + size * 3, y - size),       # Top-right
         (x + size * 3, y + size * 3),   # Bottom-right
-        (x, y + size * 4),              # Bottom-middle
+        (x, y + side),                  # Bottom-middle
     ]
 
     draw.polygon(cube_coords[:4], fill=color, outline=outline) # Front face
