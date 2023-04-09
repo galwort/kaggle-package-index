fcbe6c5c2e1b004a9d420be052093d699d961ff2
galwort <wthomasgorbett@gmail.com>
2023-04-09 09:53:21 -0400

    Remove math  and calculate angle manually

--- kpi-logo.py
+++ kpi-logo.py
@@ -1,5 +1,4 @@
 from PIL import Image, ImageDraw
-from math import cos, radians
 
 
 def shade_color(color, shade=0.85):
@@ -14,22 +13,25 @@ def make_cube(
     color=(255, 255, 255),
     outline=(0, 0, 0),
     shaded=False,
-    vertical_perspective=0,
+    vertical_perspective=45,
 ):
+    if vertical_perspective < 0:
+        vertical_perspective = 0
+    elif vertical_perspective > 90:
+        vertical_perspective = 90
+    vc = vertical_perspective / 90
+
     img = Image.open(image)
     img = Image.new("RGB", img.size, color="white")
     draw = ImageDraw.Draw(img)
 
-    rad_vp = radians(vertical_perspective)
-    scaling = cos(rad_vp)
-
     cube_coords = [
         (x, y),  # Middle
-        (x + edge_size, y),  # Top-right
-        (x + edge_size, y + edge_size),  # Bottom-right
+        (x + edge_size, y - vc * edge_size),  # Top-right
+        (x + edge_size, y + edge_size - vc * edge_size),  # Bottom-right
         (x, y + edge_size),  # Bottom-middle
-        (x - edge_size, y + edge_size),  # Bottom-left
-        (x - edge_size, y),  # Top-left
+        (x - edge_size, y + edge_size - vc * edge_size),  # Bottom-left
+        (x - edge_size, y - vc * edge_size),  # Top-left
     ]
 
     draw.polygon(cube_coords[:4], fill=color, outline=outline)  # Front face
@@ -41,4 +43,4 @@ def make_cube(
     img.save("kpi-logo.png")
 
 
-make_cube("kpi-logo.png", 200, 200, shaded=True)
+make_cube("kpi-logo.png", 200, 200, 100, shaded=True)
