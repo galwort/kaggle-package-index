f73597239a566a2b182ce4c27d35d213b4596c29
galwort <wthomasgorbett@gmail.com>
2023-04-25 14:30:56 -0400

    Clean up plot by ordering dataframe

--- kpi-stats.py
+++ kpi-stats.py
@@ -33,14 +33,18 @@ def get_pypi_downloads(packages, with_mirrors=True):
 
 def plot_pypi_downloads(pypi_df):
     fig, ax = plt.subplots(figsize=(16, 9))
+    pypi_df = pypi_df.sort_values(by=["package", "date"])
     for package in pypi_df["package"].unique():
         ax.plot(
             pypi_df[pypi_df["package"] == package]["date"],
             pypi_df[pypi_df["package"] == package]["downloads"],
             label=package,
         )
+    ax.set_xlabel("Date")
+    ax.set_ylabel("Downloads")
+    ax.set_title("Package Popularity Over Time")
     ax.legend()
     plt.show()
 
 
-plot_pypi_downloads(get_pypi_downloads(get_kaggle_packages()))
+plot_pypi_downloads(get_pypi_downloads(get_kaggle_packages(10)))
